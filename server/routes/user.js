e4ad8900fb44243d7d1fedff4d5f2529b1da6835
vagrant <vagrant@homestead>
2021-10-31 20:50:53 +0300

    es6

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
-const { Router } = require('express')
+import { Router } from 'express'
 const router = Router()
-const UserModel = require('../../app/Models/User')
+import UserModel from '../../app/Models/User'
 // const UserController = require('../controllers/user')
 // const user = new UserController()
+
 router.post('/all-users',  async (req, res) => {
      res.json(await UserModel.findAll());
 })
 
-module.exports = router
+// module.exports = router
 
